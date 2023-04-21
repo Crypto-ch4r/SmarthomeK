4c4adbeed4fb92e5c9e75ac024cf761413319f43
Charlie <carlos.zendejasmtz@gmail.com>
2023-04-20 18:40:21 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/cdhidalgo/MainActivity.kt
@@ -0,0 +1,60 @@
+package mx.tecnm.cdhidalgo
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+        private lateinit var etUsername: EditText
+        private lateinit var etPassword: EditText
+        private lateinit var bLogin: Button
+
+        var sesion: SharedPreferences? = null
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+
+                etUsername = findViewById(R.id.etUsername)
+                etPassword = findViewById(R.id.etPassword)
+                bLogin = findViewById(R.id.bLogin)
+                //archivo xml para guardar el token
+                sesion = getSharedPreferences("sesion",0)
+                bLogin.setOnClickListener{login()}
+        }
+        private fun login() {
+                var url = Uri.parse("http://172.18.7.251/login")
+                        .buildUpon()
+                        .build().toString()
+
+                val dato: JSONObject = JSONObject()
+                dato.put("username",etUsername?.text.toString())
+                dato.put("password",etPassword?.text.toString())
+
+                val peticion = JsonObjectRequest(
+                        Request.Method.POST, url, dato,
+                        {
+                                res -> respuesta(res)
+                        },
+                        {
+                                Toast.makeText(this, "Error en la peticion", Toast.LENGTH_SHORT).show()
+                        })
+                MySingleton.getInstance(applicationContext).addToRequestQueue(peticion)
+        }
+
+        private fun respuesta(res: JSONObject) {
+                with(sesion!!.edit()){
+                        putString("token", res.getString("jwt"))
+                        putString("user", etUsername.text.toString())
+                        apply()
+                }
+                startActivity(Intent(this, MainActivity2::class.java))
+        }
+}
